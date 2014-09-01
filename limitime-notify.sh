d434176aeb6bc3999ae94076ead28fd708bba1ba
Nawfel <b.gh.n.93@gmail.com>
2014-09-01 18:05:40 +0100

    Quoting: Changes suggested by shellcheck

--- limitime-notify.sh
+++ limitime-notify.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-nth_or() { (cat; echo "" )|head -n $1 | tail -n 1 | sed 's/^\s*$/'$2'/'; }
+nth_or() { (cat; echo)|head -n "$1" | tail -n 1 | sed 's/^\s*$/'"$2"'/'; }
 
-critical=$1
+critical="$1"
 if [ -z "$critical" ]
 then
     critical=5
 fi
-echo critical $critical
+echo critical "$critical"
 username=$(whoami)
-echo username $username
+echo username "$username"
 while true
 do
-    consumed=$(cat consumed/$username)
-    echo consumed $consumed
+    consumed=$(cat "consumed/$username")
+    echo consumed "$consumed"
     weekday=$(date +%u)
-    echo weekday $weekday
-    allowed=$(cat limits/$username|nth_or $weekday 9999) # 9999 is infinity
-    echo allowed $allowed
-    remaining=$(($allowed - $consumed))
-    echo remaining $remaining
-    if [ $(($remaining <= $critical)) = 1 ]
+    echo weekday "$weekday"
+    allowed=$(nth_or "$weekday" 9999 < "limits/$username") # 9999 is infinity
+    echo allowed "$allowed"
+    remaining=$((allowed - consumed))
+    echo remaining "$remaining"
+    if [ $((remaining <= critical)) = 1 ]
     then
         notify-send "Remaining $remaining minutes"
     fi
--- limitime.sh
+++ limitime.sh
@@ -6,29 +6,29 @@ umask 002
 
 # echo the $1 nth line of stdin
 # if that line doesn't exist than echo $2
-nth_or() { (cat; echo; )|head -n $1 | tail -n 1 | sed 's/^\s*$/'$2'/'; }
+nth_or() { (cat; echo;)|head -n "$1" | tail -n 1 | sed 's/^\s*$/'"$2"'/'; }
 
 for username in $(ls /home)
 do
-    if [ -e limits/$username ] && # if a roule is defined for user
-        { who | grep '^'$username > /dev/null; } # user is logged on 
+    if [ -e "limits/$username" ] && # if a roule is defined for user
+        { who | grep "^$username" > /dev/null; } # user is logged on 
     then
-        if [ ! -e consumed/$username ] ||
-            [ $(( $(date -d $(date +%Y/%m/%d) +%s) >
-                  $(stat consumed/$username -c %Y) )) == 1 ] # consumed/$username was not modified today
+        if [ ! -e "consumed/$username" ] ||
+            [ $(( $(date -d "$(date +%Y/%m/%d)" +%s) >
+                  $(stat "consumed/$username" -c %Y) )) == 1 ] # consumed/$username was not modified today
         then
-            echo 0 > consumed/$username
+            echo 0 > "consumed/$username"
         fi
         weekday=$(date +%u)
         # blank lines are interpreted as INFINITY (there are less than 9999min in a day)
-        timepermited=$(cat limits/$username|nth_or $weekday 9999) 
-        timeconsumed=$(cat consumed/$username)
-        if [ $(($timeconsumed >= $timepermited)) = 1 ]
+        timepermited=$(nth_or "$weekday" 9999 < "limits/$username") 
+        timeconsumed=$(cat "consumed/$username")
+        if [ $((timeconsumed >= timepermited)) = 1 ]
         then
             # exit users session
-            pkill -u $username -KILL
+            pkill -u "$username" -KILL
         else
-            echo $(($timeconsumed+1)) > consumed/$username
+            echo $((timeconsumed + 1)) > "consumed/$username"
         fi
     fi
 done
